f00efc50f7996465dda39d2ccc4fd1ed6eec323f
KAnggara75 <KAnggara75@gmail.com>
2025-11-06 05:55:57 +0700

    Thu Nov  6 05:55:57 WIB 2025

---
+++ backend/internal/repository/db.go
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2025 KAnggara75
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * See <https://www.gnu.org/licenses/gpl-3.0.html>.
+ *
+ * @author KAnggara75 on Thu 06/11/25 05.55
+ * @project backend repository
+ * https://github.com/KAnggara75/sentinel/tree/main/backend/internal/repository
+ */
+
+package repository
